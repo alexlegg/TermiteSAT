45c26ee60e2fd32f6042ed04206cdc6faa8ed85f
Alex Legg <alexander.legg@nicta.com.au>
2014-10-03 10:29:11 +1000

    command line options

--- Main.hs
+++ Main.hs
@@ -1,6 +1,34 @@
+import System.Environment
+import System.Console.GetOpt
+import Control.Monad.IO.Class
+
 import Expression.Expression
 import Expression.Parse
 
+data Option = InputFile String
+            | Bound String
+
+data Config = Config { tslFile      :: String
+                     , bound        :: Int
+                     } deriving (Show, Eq)
+
+defaultConfig = Config {
+    tslFile     = "",
+    bound       = 3
+    }
+
+options =
+    [ Option ['k']  ["bound"]   (ReqArg Bound "K") "Bounded reachability unroll length"]
+
+addOption (InputFile fn) c  = c {tslFile = fn}
+addOption (Bound k) c       = c {bound = (read k)}
+
 main = do
-    putStrLn "TermiteSAT"
+    args <- liftIO getArgs
+    let config = addOption (InputFile (last args)) defaultConfig
+    config <- case getOpt Permute options args of
+        (o, n, [])  -> return $ foldr addOption config o
+        _           -> error "Invalid Options"
+
+    putStrLn (show config)
 
