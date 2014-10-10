cd316846f3596c571dd8a63bf6aedcf8bf5c0200
Alex Legg <alexander.legg@nicta.com.au>
2014-10-10 16:02:34 +1100

    Build glucose with cabal

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,10 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils (rawSystemExit)
+
+main = defaultMainWithHooks simpleUserHooks
+        { preBuild = \a b -> makeGlucose a b >> preBuild simpleUserHooks a b }
+
+makeGlucose _ flags = 
+    rawSystemExit (fromFlag $ buildVerbosity flags) "sh"
+        ["-c", "(cd ./glucose/simp; LIB=glucose make libr)"]
