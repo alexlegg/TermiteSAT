f36399701ad1cb072cf068d36881de982a86650a
Alex Legg <alexander.legg@nicta.com.au>
2014-10-31 15:14:04 +1100

    Fixed ranks to only decrease on Universal nodes

--- Synthesise/GameTree.hs
+++ Synthesise/GameTree.hs
@@ -3,6 +3,8 @@ module Synthesise.GameTree (
     , GTCrumb(..)
     , GameTree(..)
     , Assignment(..)
+    , Player(..)
+    , opponent
     , gtrank
     , gtcopy
     , gtroot
@@ -23,11 +25,17 @@ import Expression.Expression
 
 data Assignment = Assignment Sign ExprVar deriving (Eq, Ord)
 
+data Player = Existential | Universal deriving (Show, Eq)
+
+opponent Existential    = Universal
+opponent Universal      = Existential
+
 instance Show Assignment where
     show (Assignment Pos v) = show v
     show (Assignment Neg v) = '-' : show v
 
 data GTNode = GTNode {
+    player      :: Player,
     treerank    :: Int,
     copy        :: Int,
     subtrees    :: Map.Map [Assignment] GTNode
@@ -57,9 +65,10 @@ update :: (GTNode -> GTNode) -> GameTree -> GameTree
 update f (n, [])        = (f n, [])
 update f (n, (a:as))    = let n' = n {subtrees = Map.adjust (\x -> fst $ update f (x, as)) a (subtrees n)} in (n', a:as)
 
-empty :: Int -> GameTree
-empty r = (node, []) 
+empty :: Player -> Int -> GameTree
+empty p r = (node, []) 
     where node = GTNode {
+        player   = p,
         treerank = r,
         copy     = 0,
         subtrees = Map.empty
@@ -80,8 +89,10 @@ appendChild :: GameTree -> [Assignment] -> GameTree
 appendChild gt a = update insert gt
     where
         insert g    = g {subtrees = Map.insert a (child g) (subtrees g)}
+        newrank n   = (treerank n) - if player n == Universal then 1 else 0
         child n     = GTNode {
-                        treerank    = (treerank n) - 1,
+                        player      = opponent (player n),
+                        treerank    = newrank n,
                         copy        = (copy n) + 1,
                         subtrees    = Map.empty }
 
